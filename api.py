1588a166de68525940e157c0d31e96cb390e48ed
Hari Murugan Ravindran <ping2harimurugan@gmail.com>
2022-12-01 01:16:09 -0700

    commitnow

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Resource,Api,reqparse
-import pandas
+import pandas as pd
 import ast
 
 app = Flask(__name__)
@@ -8,11 +8,16 @@ api = Api(app)
 
 #/users
 
-users_path = 'data/users.csv'
+users_path = './data/users.csv'
 #/locations
 
 class Users(Resource):
     def get(self):
+        data = pd.read_csv(users_path)
+        data = data.to_dict()
+        return{'data':data},200
+
+    
 
 
 
@@ -25,4 +30,4 @@ api.add_resource(Locations,'/locations')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
